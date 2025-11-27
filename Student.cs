aebe79044d3d958ba7c4acbffe491fb7ae2a1cb8
quetran2509005-alt <quetran2509005@gmail.com>
2025-11-27 12:45:06 +0700

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAITAP
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"ID: {Id}, Name: {Name}, Age: {Age}");
+        }
+    }
+}
